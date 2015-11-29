3b1a96027e75ba0ae3a9d414c3a847deacab14cd
Jackie Ho <jackieho@Jackies-MacBook-Pro.local>
2015-11-29 18:01:32 -0500

    Initial commit

---
+++ app/src/main/java/com/jackieho/grocerme/Login.java
@@ -0,0 +1,59 @@
+package com.jackieho.grocerme;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.content.Intent;
+
+public class Login extends AppCompatActivity implements View.OnClickListener {
+
+    Button bLogin;
+    EditText myUsername, myPass;
+    TextView myRegisterLink;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        myUsername = (EditText) findViewById(R.id.myUsername);
+        myPass = (EditText) findViewById(R.id.myPass);
+        bLogin = (Button) findViewById(R.id.bLogin);
+        myRegisterLink = (TextView) findViewById(R.id.myRegisterLink);
+
+        bLogin.setOnClickListener(this);
+        myRegisterLink.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch(v.getId()) {
+            case R.id.bLogin:
+                break;
+            case R.id.myRegisterLink:
+                startActivity(new Intent(this, Register.class));
+                break;
+        }
+        /**
+        if (v.getId() == R.id.bLogin) {
+
+            startActivity(new Intent(this, MainActivity.class));
+
+        }
+        else if (v.getId() == R.id.myRegisterLink) {
+
+            startActivity(new Intent(this, Register.class));
+
+        }
+         */
+
+    }
+
+}
---
+++ app/src/main/java/com/jackieho/grocerme/Register.java
@@ -0,0 +1,45 @@
+package com.jackieho.grocerme;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.content.Intent;
+
+public class Register extends AppCompatActivity implements View.OnClickListener {
+
+    Button bRegister;
+    EditText myName, myUsername, myPass, myConfirmPass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        myName = (EditText) findViewById(R.id.myName);
+        myUsername = (EditText) findViewById(R.id.myUsername);
+        myPass = (EditText) findViewById(R.id.myPass);
+        myConfirmPass = (EditText) findViewById(R.id.myConfirmPass);
+        bRegister = (Button) findViewById(R.id.bRegister);
+
+        bRegister.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v.getId() == R.id.bRegister) {
+
+            startActivity(new Intent(this, MainActivity.class));
+
+        }
+
+    }
+
+}
